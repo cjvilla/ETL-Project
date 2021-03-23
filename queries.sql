74faddeb99e79a33f4962d6b21d532308f433e3f
cjvilla <CelineVillareal2020@northwestern.edu>
2021-03-23 14:34:33 -0500

    added filter queries on data

--- queries.sql
+++ queries.sql
@@ -20,3 +20,37 @@ select us.id, us.neighbourhood, us.room_type, us.price, us.minimum_nights, us.ci
 from us_data as us
 join chicago_data as c
 on us.id = c.id;
+
+select * from us_data;
+select * from avg_price_by_city;
+select * from count_by_city;
+select * from count_room_and_avg_price;
+select * from max_price_by_city; 
+
+-- cities with the most airbnbs in desc
+create view count_by_city as
+select city, count(city) as city_count
+from us_data
+group by city
+order by city_count desc;
+
+-- max price ber city
+create view max_price_by_city as
+select city, max(price) as max_price
+from us_data
+group by city
+order by max_price desc;
+
+-- avg price per city
+create view avg_price_by_city as
+select city, cast(avg(price) as decimal(16,2)) as avg_price, cast(avg(minimum_nights) as decimal(16,2)) as minimum_nights
+from us_data
+group by city
+order by avg_price desc;
+
+-- count of room type per city
+create view count_room_and_avg_price as
+select room_type, count(room_type) as count_per_room_type, cast(avg(price) as decimal(16,2)) as avg_price
+from us_data
+group by room_type
+order by count_per_room_type desc;
