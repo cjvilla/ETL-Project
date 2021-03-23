62407d7024a6ebec9e4963969e45673e447a78fd
cjvilla <CelineVillareal2020@northwestern.edu>
2021-03-22 22:13:40 -0500

    Uploaded queries to create tables

---
+++ queries.sql
@@ -0,0 +1,17 @@
+create table us_data (
+	id int primary key,
+	neighbourhood text,
+	room_type text,
+	price int,
+	minimum_nights int,
+	city text
+);
+
+create table chicago_data (
+	id int primary key,
+	neighbourhood text,
+	room_type text,
+	price int,
+	minimum_nights int,
+	city text
+);
